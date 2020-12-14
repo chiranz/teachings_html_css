c45ec064cfa8a79bf6d991378eb89b7a07d5a33e
chiranz <chiranpoudyal@gmail.com>
2020-12-14 16:10:02 +0530

    Dec 14 Css Grid

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-let variable = "variable";
+let myBool = true | false;
+// integer ranges from 0-9
+let myInt = 12134567876543212345;
+// string
+let soe = "my string";
+let myCart = [];
+let myObj = {
+  name: "myname",
+  age: 12,
+};
+
+myCart.push({ itemName: "First Item", price: 12, quantity: 2 });
+myCart.push({ itemName: "Second Item", price: 12, quantity: 2 });
+myCart.push({ itemName: "Third Item", price: 12, quantity: 2 });
+myCart.push({ itemName: "Fourth Item", price: 12, quantity: 2 });
+
+myCart.sort();
+console.log(myCart);
+
+let name = "Something";

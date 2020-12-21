d24530da3437ceca1ac911021f06ddb2c0829f58
chiranz <chiranpoudyal@gmail.com>
2020-12-21 16:29:45 +0530

    21 dec 2020

--- main.js
+++ main.js
@@ -1,20 +1,59 @@
-let myBool = true | false;
-// integer ranges from 0-9
-let myInt = 12134567876543212345;
-// string
-let soe = "my string";
-let myCart = [];
-let myObj = {
-  name: "myname",
-  age: 12,
+function whereIsSimAvalilable() {
+  let simInRhenock = false;
+  let simInKpg = false;
+  let simInSiliguri = true;
+  if (simInRhenock === true) {
+    console.log("SIM found in rhenock");
+  } else {
+    console.log("Sim not found in rhenock!");
+  }
+  if (simInKpg === true) {
+    console.log("SIM found in KPG");
+  } else {
+    console.log("Sim not found in KPG!");
+  }
+  if (simInSiliguri === true) {
+    console.log("SIM found in Siliguri");
+  } else {
+    console.log("Sim not found in Siliguri!");
+  }
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// Function with logical operators and conditionals
+function calculator(task, a, b) {
+  if (task === "add") {
+    return add(a, b);
+  } else if (task === "substract") {
+    return substract(a, b);
+  } else if (task === "divide") {
+    return divide(a, b);
+  } else if (task === "multiply") {
+    return multiply(a, b);
+  } else {
+    return "We cannot do that task";
+  }
+}
+
+// DATA STRUCTURES
+// Array
+let masalaContainer = ["5foran", "haldi", "methi", "nun", "ginger", "garlic"];
+// Dictonary / Object
+
+let masalaObject = {
+  methi: [],
 };
-
-myCart.push({ itemName: "First Item", price: 12, quantity: 2 });
-myCart.push({ itemName: "Second Item", price: 12, quantity: 2 });
-myCart.push({ itemName: "Third Item", price: 12, quantity: 2 });
-myCart.push({ itemName: "Fourth Item", price: 12, quantity: 2 });
-
-myCart.sort();
-console.log(myCart);
-
-let name = "Something";

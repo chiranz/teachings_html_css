e061f927b0a2af528a6b4d0aefa331c595801c26
chiranz <chiranpoudyal@gmail.com>
2021-01-14 19:49:32 +0530

    Functions and intro to classes 14th Jan 2021

--- main.js
+++ main.js
@@ -57,3 +57,37 @@ let masalaContainer = ["5foran", "haldi", "methi", "nun", "ginger", "garlic"];
 let masalaObject = {
   methi: [],
 };
+
+// JAN 14 2021
+
+function Player(name, suit, weapons) {
+  let player = {
+    health: 100,
+    name: name,
+    suit: suit ? suit : "Normal suit",
+    weapons: weapons ? weapons : "Normal Weapons",
+  };
+
+  player.eat = (amount) => {
+    player.health += amount;
+  };
+  player.getHealth = () => {
+    console.log(`Remaining health of player is ${player.health}`);
+  };
+
+  player.shoot = () => {
+    console.log(`Player is shoooting`);
+  };
+
+  player.changeWeapon = (weapon) => {
+    player.weapons = weapon;
+  };
+  player.changeSuit = () => {};
+  // TODO: get current suit of player
+  // TODO: Set current Suit of player
+  // TODO: Reduce health of player
+  // TODO: What does new keyword mean in javascript
+  // TODO: What does this keyword mean in javascript
+
+  return player;
+}

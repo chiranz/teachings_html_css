9ce25b18a047ab9c95cd426a0f9f2c8d1573e705
chiranz <chiranpoudyal@gmail.com>
2021-04-15 14:02:18 +0530

    Api calls with fetch

--- main.js
+++ main.js
@@ -1,93 +1,23 @@
-function whereIsSimAvalilable() {
-  let simInRhenock = false;
-  let simInKpg = false;
-  let simInSiliguri = true;
-  if (simInRhenock === true) {
-    console.log("SIM found in rhenock");
-  } else {
-    console.log("Sim not found in rhenock!");
-  }
-  if (simInKpg === true) {
-    console.log("SIM found in KPG");
-  } else {
-    console.log("Sim not found in KPG!");
-  }
-  if (simInSiliguri === true) {
-    console.log("SIM found in Siliguri");
-  } else {
-    console.log("Sim not found in Siliguri!");
-  }
-}
+// Javascript CHEATCODEURL
+// https://gist.github.com/thegitfather/9c9f1a927cd57df14a59c268f118ce86
+const postsDiv = document.getElementById("posts");
 
-function multiply(a, b) {
-  return a * b;
-}
-function add(a, b) {
-  return a + b;
-}
-
-function substract(a, b) {
-  return a - b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-// Function with logical operators and conditionals
-function calculator(task, a, b) {
-  if (task === "add") {
-    return add(a, b);
-  } else if (task === "substract") {
-    return substract(a, b);
-  } else if (task === "divide") {
-    return divide(a, b);
-  } else if (task === "multiply") {
-    return multiply(a, b);
-  } else {
-    return "We cannot do that task";
-  }
-}
-
-// DATA STRUCTURES
-// Array
-let masalaContainer = ["5foran", "haldi", "methi", "nun", "ginger", "garlic"];
-// Dictonary / Object
-
-let masalaObject = {
-  methi: [],
+const getPosts = async () => {
+  postsDiv.innerText = "Loading...";
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+  const singlePost = posts[0];
+  const singlePostDiv = document.createElement("div");
+  const postHeading = document.createElement("h1");
+  const postContent = document.createElement("p");
+  const postHeadingText = document.createTextNode(singlePost.title);
+  const postBodyText = document.createTextNode(singlePost.body);
+  postsDiv.innerText = "";
+  postHeading.appendChild(postHeadingText);
+  postContent.appendChild(postBodyText);
+  singlePostDiv.appendChild(postHeading);
+  singlePostDiv.appendChild(postContent);
+  postsDiv.appendChild(singlePostDiv);
 };
 
-// JAN 14 2021
-
-function Player(name, suit, weapons) {
-  let player = {
-    health: 100,
-    name: name,
-    suit: suit ? suit : "Normal suit",
-    weapons: weapons ? weapons : "Normal Weapons",
-  };
-
-  player.eat = (amount) => {
-    player.health += amount;
-  };
-  player.getHealth = () => {
-    console.log(`Remaining health of player is ${player.health}`);
-  };
-
-  player.shoot = () => {
-    console.log(`Player is shoooting`);
-  };
-
-  player.changeWeapon = (weapon) => {
-    player.weapons = weapon;
-  };
-  player.changeSuit = () => {};
-  // TODO: get current suit of player
-  // TODO: Set current Suit of player
-  // TODO: Reduce health of player
-  // TODO: What does new keyword mean in javascript
-  // TODO: What does this keyword mean in javascript
-
-  return player;
-}
+getPosts();
